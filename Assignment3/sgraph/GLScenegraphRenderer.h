4f41da0eaf1fa3ea25e970c3ff34bce122570cf9
Wycode-fish <164335424@qq.com>
2019-03-28 23:55:44 -0400

    sgraph

---
+++ Assignment3/sgraph/GLScenegraphRenderer.h
@@ -0,0 +1,265 @@
+#ifndef _GLSCENEGRAPHRENDERER_H_
+#define _GLSCENEGRAPHRENDERER_H_
+
+#include "INode.h"
+#include "OpenGLFunctions.h"
+#include "glm/glm.hpp"
+#include <glm/gtc/type_ptr.hpp>
+#include "Material.h"
+#include "TextureImage.h"
+#include "ObjectInstance.h"
+#include "IVertexData.h"
+#include "ShaderLocationsVault.h"
+#include <string>
+#include <map>
+#include <stack>
+using namespace std;
+
+namespace sgraph
+{
+
+/**
+ * This is a scene graph renderer implementation that works specifically
+ * with the Qt library
+ * It mandates OpenGL 3 and above.
+ * \author Amit Shesh
+ */
+class GLScenegraphRenderer
+{
+    /**
+     * The Qt specific rendering context
+     */
+private:
+    util::OpenGLFunctions *glContext;
+    /**
+     * A table of shader locations and variable names
+     */
+protected :
+    util::ShaderLocationsVault shaderLocations;
+    /**
+     * A table of shader variables -> vertex attribute names in each mesh
+     */
+    map<string,string> shaderVarsToVertexAttribs;
+
+    /**
+     * A map to store all the textures
+     */
+    map<string, util::TextureImage *> textures;
+    /**
+     * A table of renderers for individual meshes
+     */
+    map<string, util::ObjectInstance *> meshRenderers;
+
+    /**
+     * A variable tracking whether shader locations have been set. This must be done before
+     * drawing!
+     */
+    bool shaderLocationsSet;
+
+public:
+    GLScenegraphRenderer()
+    {
+        shaderLocationsSet = false;
+    }
+
+    /**
+     * Specifically checks if the passed rendering context is the correct JOGL-specific
+     * rendering context
+     * \param obj the rendering context (should be OpenGLFunctions)
+     */
+    void setContext(util::OpenGLFunctions *obj)
+    {
+        glContext = obj;
+    }
+
+    /**
+     * Add a mesh to be drawn later.
+     * The rendering context should be set before calling this function, as this function needs it
+     * This function creates a new sgraph::GLMeshRenderer object for this mesh
+     * \param name the name by which this mesh is referred to by the scene graph
+     * \param mesh the util::PolygonMesh object that represents this mesh
+     */
+    template <class K>
+    void addMesh(const string& name,
+                 util::PolygonMesh<K>& mesh) throw(runtime_error)
+    {
+        if (!shaderLocationsSet)
+            throw runtime_error("Attempting to add mesh before setting shader variables. Call initShaderProgram first");
+        if (glContext==NULL)
+            throw runtime_error("Attempting to add mesh before setting GL context. Call setContext and pass it a GLAutoDrawable first.");
+
+        //verify that the mesh has all the vertex attributes as specified in the map
+        if (mesh.getVertexCount()<=0)
+            return;
+        K vertexData = mesh.getVertexAttributes()[0];
+        for (map<string,string>::iterator it=shaderVarsToVertexAttribs.begin();
+             it!=shaderVarsToVertexAttribs.end();it++) {
+            if (!vertexData.hasData(it->second))
+                throw runtime_error("Mesh does not have vertex attribute "+it->second);
+        }
+        util::ObjectInstance *mr = new util::ObjectInstance(name);
+        mr->initPolygonMesh<K>(*glContext,
+                            shaderLocations,
+                            shaderVarsToVertexAttribs,
+                            mesh);
+        this->meshRenderers[name] = mr;
+    }
+
+    void addTexture(const string& name,const string& path)
+    {
+        util::TextureImage *image = NULL;
+        try {
+            image = new util::TextureImage(path,name);
+        } catch (runtime_error e) {
+            throw runtime_error("Texture "+path+" cannot be read!");
+        }
+        textures[name]=image;
+    }
+
+    /**
+     * Begin rendering of the scene graph from the root
+     * \param root
+     * \param modelView
+     */
+    void draw(INode *root, stack<glm::mat4>& modelView)
+    {
+        root->draw(*this,modelView);
+    }
+
+    void drawLight(INode *root, stack<glm::mat4>& _mv)
+    {
+        std::vector<util::Light> lights;
+        root->drawLight(*this, _mv, lights);
+    }
+
+    void dispose()
+    {
+        for (map<string,util::ObjectInstance *>::iterator it=meshRenderers.begin();
+             it!=meshRenderers.end();it++)
+          {
+            it->second->cleanup(*glContext);
+          }
+    }
+    /**
+     * Draws a specific mesh.
+     * If the mesh has been added to this renderer, it delegates to its correspond mesh renderer
+     * This function first passes the material to the shader. Currently it uses the shader variable
+     * "vColor" and passes it the ambient part of the material. When lighting is enabled, this method must
+     * be overriden to set the ambient, diffuse, specular, shininess etc. values to the shader
+     * \param name
+     * \param material
+     * \param transformation
+     */
+    void drawMesh(const string& name,
+                  const util::Material& material,
+                  const string& textureName,
+                  const glm::mat4& transformation)
+    {
+        if (meshRenderers.count(name)==1)
+        {
+//            int loc = shaderLocations.getLocation("vColor");
+//            //set the color for all vertices to be drawn for this object
+//            if (loc<0)
+//                throw runtime_error("No shader variable for \" vColor \"");
+
+//            glContext->glUniform3fv(loc,1,glm::value_ptr(material.getAmbient()));
+
+            /*Material properties are now being sent to the shader*/
+            glContext->glUniform3fv(shaderLocations.getLocation("material.ambient"), 1, glm::value_ptr(material.getAmbient()));
+            glContext->glUniform3fv(shaderLocations.getLocation("material.diffuse"), 1, glm::value_ptr(material.getDiffuse()));
+            glContext->glUniform3fv(shaderLocations.getLocation("material.specular"), 1,glm::value_ptr(material.getSpecular()));
+            glContext->glUniform1f(shaderLocations.getLocation("material.shininess"), material.getShininess());
+
+            /*Sending the final modelview matrix which contains all the transformations from the root of the scenegraph to the respective
+              leaf node.*/
+            int loc = shaderLocations.getLocation("modelview");
+            if (loc<0)
+                throw runtime_error("No shader variable for \" modelview \"");
+            glContext->glUniformMatrix4fv(loc,1,false,glm::value_ptr(transformation));
+
+            /*Creating and sending the normalmatrix the shader requires. The normal matrix is the inverse-transpose of the final modelview matrix.*/
+            glm::mat4 normalmatrix = glm::inverse(glm::transpose((transformation)));
+            glContext->glUniformMatrix4fv(shaderLocations.getLocation("normalmatrix"), 1, false,glm::value_ptr(normalmatrix));
+
+
+            meshRenderers[name]->draw(*glContext);
+        }
+    }
+
+
+    void drawLight(std::vector<util::Light> lights,
+                  const glm::mat4& transformation)
+    {
+
+        struct _lightLocation
+        {
+            int ambient = -1;
+            int diffuse = -1;
+            int specular = -1;
+            int position = -1;
+        };
+
+        std::vector<_lightLocation> lls;
+        for (int i=0; i<lights.size(); ++i)
+        {
+            std::stringstream ss;
+            ss << "light[" << i << "]";
+            std::string sVarName = ss.str();
+            _lightLocation ll;
+            ll.ambient = shaderLocations.getLocation(sVarName + ".ambient");
+            ll.diffuse = shaderLocations.getLocation(sVarName + ".diffuse");
+            ll.specular = shaderLocations.getLocation(sVarName + ".specular");
+            ll.position = shaderLocations.getLocation(sVarName + ".position");
+            lls.push_back(ll);
+        }
+            /*Light properties are now being sent to the shader*/
+        glContext->glUniform1i(shaderLocations.getLocation("numLights"), lights.size());
+
+        for (int i=0; i<lights.size(); ++i)
+        {
+
+            glContext->glUniform3fv(lls[i].ambient, 1, glm::value_ptr(lights[i].getAmbient()));
+            glContext->glUniform3fv(lls[i].specular, 1, glm::value_ptr(lights[i].getSpecular()));
+            glContext->glUniform3fv(lls[i].diffuse, 1, glm::value_ptr(lights[i].getDiffuse()));
+            glContext->glUniform4fv(lls[i].position, 1, glm::value_ptr(glm::vec4(lights[i].getPosition())));
+
+        }
+
+            /*Sending the final modelview matrix which contains all the transformations from the root of the scenegraph to the respective
+              leaf node.*/
+//            int loc = shaderLocations.getLocation("modelview");
+//            if (loc<0)
+//                throw runtime_error("No shader variable for \" modelview \"");
+//            glContext->glUniformMatrix4fv(loc,1,false,glm::value_ptr(transformation));
+
+//            /*Creating and sending the normalmatrix the shader requires. The normal matrix is the inverse-transpose of the final modelview matrix.*/
+//            glm::mat4 normalmatrix = glm::inverse(glm::transpose((transformation)));
+//            glContext->glUniformMatrix4fv(shaderLocations.getLocation("normalmatrix"), 1, false,glm::value_ptr(normalmatrix));
+
+//            meshRenderers[name]->draw(*glContext);
+
+    }
+
+    /**
+     * Queries the shader program for all variables and locations, and adds them to itself
+     * \param shaderProgram
+     */
+    void initShaderProgram(util::ShaderProgram& shaderProgram,
+                           map<string,string>& shaderVarsToVertexAttribs)
+    {
+        if (glContext==NULL)
+          throw runtime_error("No context set");
+
+        shaderLocations = shaderProgram.getAllShaderVariables(*glContext);
+        this->shaderVarsToVertexAttribs = shaderVarsToVertexAttribs;
+        shaderLocationsSet = true;
+
+    }
+
+    int getShaderLocation(const string& name)
+    {
+        return shaderLocations.getLocation(name);
+    }
+};
+}
+#endif
---
+++ Assignment3/sgraph/LeafNode.h
@@ -0,0 +1,120 @@
+#ifndef _LEAFNODE_H_
+#define _LEAFNODE_H_
+
+#include "AbstractNode.h"
+#include "OpenGLFunctions.h"
+#include "Material.h"
+#include "glm/glm.hpp"
+#include <map>
+#include <stack>
+#include <string>
+using namespace std;
+
+namespace sgraph
+{
+
+/**
+ * This node represents the leaf of a scene graph. It is the only type of node that has
+ * actual geometry to render.
+ * \author Amit Shesh
+ */
+class LeafNode: public AbstractNode
+{
+    /**
+     * The name of the object instance that this leaf contains. All object instances are stored
+     * in the scene graph itself, so that an instance can be reused in several leaves
+     */
+protected:
+    string objInstanceName;
+    /**
+     * The material associated with the object instance at this leaf
+     */
+    util::Material material;
+
+    util::Light light;
+
+    string textureName;
+
+public:
+    LeafNode(const string& instanceOf,sgraph::Scenegraph *graph,const string& name)
+        :AbstractNode(graph,name)
+    {
+        this->objInstanceName = instanceOf;
+    }
+	
+	~LeafNode(){}
+
+
+
+    /*
+	 *Set the material of each vertex in this object
+	 */
+    void setMaterial(const util::Material& mat) throw(runtime_error)
+    {
+        material = mat;
+    }
+
+    /**
+     * Set texture ID of the texture to be used for this leaf
+     * \param name
+     */
+    void setTextureName(const string& name) throw(runtime_error)
+    {
+        textureName = name;
+    }
+
+    /*
+     * gets the material
+     */
+    util::Material getMaterial()
+    {
+        return material;
+    }
+
+    INode *clone()
+    {
+        LeafNode *newclone = new LeafNode(this->objInstanceName,scenegraph,name);
+        newclone->setMaterial(this->getMaterial());
+        return newclone;
+    }
+
+
+    /**
+     * Delegates to the scene graph for rendering. This has two advantages:
+     * <ul>
+     *     <li>It keeps the leaf light.</li>
+     *     <li>It abstracts the actual drawing to the specific implementation of the scene graph renderer</li>
+     * </ul>
+     * \param context the generic renderer context {@link sgraph.IScenegraphRenderer}
+     * \param modelView the stack of modelview matrices
+     * \throws runtime_error
+     */
+    void draw(GLScenegraphRenderer& context,stack<glm::mat4>& modelView) throw(runtime_error)
+    {
+        if (objInstanceName.length()>0)
+        {
+            context.drawMesh(objInstanceName,material,textureName,modelView.top());
+        }
+    }
+
+    void drawLight(GLScenegraphRenderer& context,stack<glm::mat4>& modelView, std::vector<util::Light>& lights)
+    {
+        if (isLightInUse)
+        {
+            glm::vec4 pos = light.getPosition();
+            pos = modelView.top() * pos;
+            util::Light l = light;
+            l.setPosition(pos);
+            lights.push_back(l);
+        }
+        context.drawLight(lights, modelView.top());
+    }
+
+    void addLight(const util::Light& _l) throw(runtime_error)
+    {
+        isLightInUse = true;
+        light = _l;
+    }
+};
+}
+#endif
